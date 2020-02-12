cbb45293bfab8bd8adc496bc66f89ca44506c992
Jeff Horner <jhorner3208@gmail.com>
2020-02-12 13:59:58 -0600

    fix typo

--- src/utilities/validationRule.js
+++ src/utilities/validationRule.js
@@ -1,14 +1,14 @@
 /**
- * Determines what error message to display based upon the fields validation state
+ * Determines what error message to display based upon the field's validation state
  * as well as what type of form field it is.
  *
- * @return  {Array}      If applicable, it will display an array with the tailored validation
- *                       error messsage.
+ * @return  {Array}           If applicable, it will display an array with the tailored validation
+ *                            error messsage.
  *
- * @param   fieldName       The form field name we're validating
- * @param   fieldName       The fieldName we're trying to validate within the validationGroup object
- * @param   errorMessage    What error message to dispaly in the event the field isn't validated.
- * @param   validationGroup The vuelidate validationGroup.
+ * @param   validationObject  The vuelidate validation object
+ * @param   fieldName         The fieldName we're trying to validate within the validationGroup object
+ * @param   errorMessage      What error message to dispaly in the event the field isn't validated.
+ * @param   validationGroup   The vuelidate validationGroup.
  */
 export default function validationRule (validationObject, fieldName, errorMessage, validationGroup = null) {
     if (validationGroup) {
