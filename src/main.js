f9b367678e620e3a3cadef18bd1d3222643c8f69
Jeff Horner <jhorner3208@gmail.com>
2020-02-16 17:23:36 -0600

    third attempt to fix netfliy build issue with firebase es6 module imports

--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ const firebaseConfig = {
     appId: process.env.GRIDESOME_FIREBASE_APP_ID,
 };
 
-// Lazy load firebase to fix es6 moudle import issues with firebase & node
-// see https://github.com/firebase/firebase-js-sdk/issues/2222
+firebase.initializeApp(firebaseConfig);
+
 import('firebase').then(firebase => {
     firebase.initializeApp(firebaseConfig);
 });
